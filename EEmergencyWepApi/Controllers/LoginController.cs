7894986b18d4324907fad8885a41aabb5074ff57
Abdalslam Aljmal <abodajaljmal@gmail.com>
2020-10-28 16:08:49 +0300

    added data base

---
+++ EEmergencyWepApi/Controllers/LoginController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EEmergencyWepApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {  
+       
+        [HttpPost]
+        public ActionResult<bool> login(string phoneNumber, string password)
+        {
+            bool a = true;
+
+
+
+            return a;
+
+        }
+    }
+}
