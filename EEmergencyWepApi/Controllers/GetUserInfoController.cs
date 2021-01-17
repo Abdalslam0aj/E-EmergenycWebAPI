7be49d9b204e492581342577a0aa64e45fb2d6c0
Abdalslam Aljmal <abodajaljmal@gmail.com>
2021-01-17 18:44:52 +0200

    yazed alkent register

---
+++ EEmergencyWepApi/Controllers/GetUserInfoController.cs
@@ -0,0 +1,33 @@
+﻿using EEmergencyWepApi.Data.module;
+using EEmergencyWepApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EEmergencyWebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class GetUserInfoController : Controller
+    {
+        ConctionDbClass db;
+        public GetUserInfoController(ConctionDbClass db)
+        {
+            this.db = db;
+        }
+        public ActionResult<Civilian> Index([FromBody]Civilian civilian)
+        {
+            try {
+                Civilian user = db.Civilian.Find(civilian.phoneNumber);
+                return user;
+            }
+            catch (Exception e) {
+                Console.WriteLine(e);
+                return new Civilian();
+            }
+         
+        }
+    }
+}
